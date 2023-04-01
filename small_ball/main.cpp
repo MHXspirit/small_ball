ee153b4333f42dea2a48f740239ba8bbfaa73c98
MHXspirit <1603427597@qq.com>
2023-04-01 23:07:59 +0800

    进度：18/105

--- small_ball/main.cpp
+++ small_ball/main.cpp
@@ -1,4 +1,5 @@
 // 用 easyx 学习 C 语言
+// IDE：vs2022
 // 课程：《C 语言程序设计：一个小球的编程之旅》- 童晶老师
 // 视频链接：https://space.bilibili.com/652060948
 
@@ -8,34 +9,31 @@
 
 int main()
 {
-	int width = 800; // 画面宽度
-	int height = 600; // 画面高度
-	int x = width / 2; // 小球 x 坐标
-	int y = height / 2; // 小球 y 坐标
-	int radius = 20; // 小球半径
-	int vx = 3; // 小球 x 方向速度
-	int vy = 4; // 小球 y 方向速度
-	initgraph(width, height); // 新建画面
-	
+	int width = 1200; // 画面宽度
+	int height = 800; // 画面高度
+	initgraph(width, height); // 初始化画面
+
+	float blockWidth = 20; // 障碍物宽度
+	float gapHeight = height * 0.5; // 空隙高度
+	float xBlockLeft = width * 0.8; // 障碍物左侧 x 坐标
+	float xBlockRight = xBlockLeft + blockWidth; // 障碍物右侧 x 坐标
+	float yGapTop = height / 3; // 空隙顶部坐标
+	float yGapBottom = yGapTop + gapHeight; // 空隙底部坐标
+
 	while (1)
 	{
 		cleardevice(); // 清屏
-		fillcircle(x, y, radius); // 画小球
+		fillrectangle(xBlockLeft, 0, xBlockRight, yGapTop); // 画上方障碍物
+		fillrectangle(xBlockLeft, yGapBottom, xBlockRight, height); // 画下方障碍物
 
-		// 小球碰到上下左右边界，速度反向
-		if (y >= height - radius || y <= radius)
-		{
-			vy = -vy;
-		}
-		if (x >= width - radius || x <= radius)
-		{
-			vx = -vx;
-		}
+		// 障碍物从右向左移动
+		xBlockLeft -= 3;
+		xBlockRight = xBlockLeft + blockWidth;
+		if (xBlockRight <= 0)
+			xBlockLeft = width;
 
-		y = y + vy; // 更新小球 x 坐标
-		x = x + vx; // 更新小球 y 坐标
-		Sleep(30); // 暂停30ms
+		Sleep(5);
 	}
-	
+
 	return 0;
 }
